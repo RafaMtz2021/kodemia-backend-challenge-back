b7592b5c7e0ac8ec34282c9f152fece7475c08f1
LuisBalan <jlbalcz@gmail.com>
2021-12-02 16:21:01 -0600

    add getPosts.js

---
+++ getPosts.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const fs = require('fs/promises');
+const ENCODING = 'utf8';
+const app = express();
+
+app.get('/', (req, res) => {
+    
+
+
+})
--- index.js
+++ index.js
@@ -1,1 +1,5 @@
 console.log('Hello World');
+
+//añadir mi endpoint GET
+//usar base de datos creada en mi mongo  atlas
+//
