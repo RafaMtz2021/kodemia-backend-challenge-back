dcc88834fde245b2c248ece2e5aa1a21feeeb7d2
Rafael Martinez <rterranova701@gmail.com>
2021-12-02 10:25:31 -0600

    Finishing endpoint POST for creating new posts in dev.to clone

--- src/controllers/post.controller.js
+++ src/controllers/post.controller.js
@@ -21,6 +21,30 @@ async function getPost(request,response) {
     }
   }
 
+async function postPost(request,response) {
+    console.log(request.query);
+  try {
+      const newPost = request.query
+      const postPost = await post.postPost(newPost)
+      response.json({
+          success: true,
+          message: 'Post created',
+          data: {
+              post: newPost,
+          }
+      })
+  } catch (error) {
+      console.error(error);
+      response.statusCode = 500
+      response.json({
+          success: false,
+          message: 'Could not create post',
+          error,
+      })
+  }
+}
+
   module.exports = {
       getPost,
+      postPost,
   }
